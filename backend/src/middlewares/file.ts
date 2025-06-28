c80871187e5fe16a8869829b26bd5d49155c92cf
Michael Alikbarov <michael.alikbarov@yandex.ru>
2025-06-28 18:47:50 +0600

    feat: изменить наименование сохраняемого файла

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,6 +1,7 @@
 import { Request, Express } from 'express'
 import multer, { FileFilterCallback } from 'multer'
-import { join } from 'path'
+import { join, extname } from 'path'
+import { v4 as uuidv4 } from 'uuid'
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -27,7 +28,9 @@ const storage = multer.diskStorage({
         file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        cb(null, file.originalname)
+        const ext = extname(file.originalname)
+        const safeFileName = `${uuidv4()}${ext}`
+        cb(null, safeFileName)
     },
 })
 
