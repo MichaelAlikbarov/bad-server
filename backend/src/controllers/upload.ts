f7dde4055c5f804a2b7c07488d10535ee8520466
Michael Alikbarov <michael.alikbarov@yandex.ru>
2025-06-29 11:51:52 +0600

    fix: проерка на мин сайз и валидное изображение

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
+import sharp from 'sharp'
 
 export const uploadFile = async (
     req: Request,
@@ -10,7 +11,18 @@ export const uploadFile = async (
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
+
+    const MIN_SIZE = 2 * 1024
     try {
+        if (req.file.size < MIN_SIZE) {
+            return next(new BadRequestError('Файл слишком маленький (менее 2KB)'))
+        }
+
+        try {
+            await sharp(req.file.path).metadata()
+        } catch {
+            return next(new BadRequestError('Файл не является валидным изображением'))
+        }
         
         const fileName = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
